bd7a2b0fe4f08c4e4725bc47b9a8663f40e71302
Luisho <luissantaescu@gmail.com>
2024-02-28 19:46:17 +0100

    Fixed subscriber

--- Robot/main.c
+++ Robot/main.c
@@ -57,6 +57,30 @@ void on_message(struct mosquitto *mosq, void *userdata, const struct mosquitto_m
 }
 
 int main() {
+    // Inicialización de Mosquitto
+    mosquitto_lib_init();
+
+    // Crear una instancia del cliente Mosquitto
+    mosq = mosquitto_new(NULL, true, NULL);
+    if (!mosq) {
+        fprintf(stderr, "Error al crear el cliente Mosquitto.\n");
+        mosquitto_lib_cleanup();
+        return 1;
+    }
+
+    // Establecer la función de mensaje recibido
+    mosquitto_message_callback_set(mosq, on_message);
+
+    // Conectar al broker Mosquitto
+    int rc = mosquitto_connect(mosq, MQTT_HOST, MQTT_PORT, 60);
+    if (rc != MOSQ_ERR_SUCCESS) {
+        fprintf(stderr, "Error al conectar con el broker Mosquitto, código: %d\n", rc);
+        mosquitto_destroy(mosq);
+        mosquitto_lib_cleanup();
+        return 1;
+    }
+    mosquitto_subscribe(mosq, NULL, TOPIC_INST, QoS_2);
+    mosquitto_subscribe(mosq, NULL, TOPIC_VEL, QoS_2);
     // Bucle principal
     while (1) {
         // Procesar eventos de Mosquitto
